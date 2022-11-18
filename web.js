23c886a36bc51e21a4dc4b33d99be321fe1bcf3d
sudhirsingh1922 <118472029+sudhirsingh1922@users.noreply.github.com>
2022-11-18 14:12:02 +0530

    API communication script

--- web.js
+++ web.js
@@ -13,6 +13,27 @@ app.post('/', function (req, res) {
   const moves = ['L', 'R','F', 'T', 'R', 'L' , 'F','L', 'R'];
   
   // TODO add your implementation here to replace the random response
+  {
+  "_links": {
+    "self": {
+      "href": "http://34.132.232.194/india-e1770626-9800-4b33-aefb-198305d414a1"
+    }
+  },
+  "arena": {
+    "dims": [4,3], // width, height
+    "state": {
+      "https://A_PLAYERS_URL": {
+        "x": 0, // zero-based x position, where 0 = left
+        "y": 0, // zero-based y position, where 0 = top
+        "direction": "N", // N = North, W = West, S = South, E = East
+        "wasHit": false,
+        "score": 0
+      }
+      ... // also you and the other players
+    }
+  }
+}
+
   
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
