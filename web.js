77a26f4d7fd28f87d45c09cc1413b16b5c476db9
sudhirsingh1922 <118472029+sudhirsingh1922@users.noreply.github.com>
2022-11-18 14:19:30 +0530

    Update web.js

--- web.js
+++ web.js
@@ -13,13 +13,7 @@ app.post('/', function (req, res) {
   const moves = ['L', 'R','F', 'T', 'R', 'L' , 'F','L', 'R'];
   
   // TODO add your implementation here to replace the random response
-  {
-  "_links": {
-    "self": {
-      "href": "http://34.132.232.194/india-e1770626-9800-4b33-aefb-198305d414a1"
-    }
-  },
-  
+
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
